9c500030bff31de8bdbc9577fd7410a87f7a0964
Mathew <mathew.corm@gmail.com>
2017-06-23 12:21:55 -0400

    Multiple repo stats

--- server/providers/github-stats-provider.ts
+++ server/providers/github-stats-provider.ts
@@ -4,6 +4,7 @@ import * as dbFactory from 'mongo-factory'
 import { BitcoinWallet, PeriodicPayment, PeriodicPaymentOptions, FixedPayment, FixedPaymentOptions, Wallet, RepoParams, RepoAuthor, RepoStats } from 'models'
 import { WalletProvider } from './wallet-provider'
 import * as BigNumber from 'bignumber.js'
+import * as _ from 'lodash'
 
 
 export class GithubStatsProvider {
@@ -23,13 +24,32 @@ export class GithubStatsProvider {
         return this.convertMongoRepoStats(stats)
     }
 
-    async getMultipleRepoStats(repos: RepoParams[]): Promise<RepoStats[]> {
+    async getMultipleRepoStats(repos: RepoParams[]): Promise<RepoStats> {
         let db = await dbFactory.getConnection(config.get('database'))
         let col = db.collection('repo-stats') as Collection
 
-        let stats = await col.find({ $or: repos }).toArray()
+        let stats = (await col.find({ $or: repos })
+                             .toArray())
+                             .map(this.convertMongoRepoStats)
 
-        return stats.map(this.convertMongoRepoStats)   
+        let authors = _(stats)
+                        .flatMap(author => author.authors)
+                        .groupBy('email')
+                        .map(authorStats => ({
+                            name: authorStats[0].name,
+                            email: authorStats[0].email,
+                            linesOfCode: authorStats.reduce((totalLines, authorStats) => totalLines.plus(authorStats.linesOfCode), new BigNumber(0)).toString(),
+                            fileCount: authorStats.reduce((totalFiles, authorStats) => totalFiles.plus(authorStats.fileCount), new BigNumber(0)).toString(),
+                            availablePaymentMethods: authorStats[0].availablePaymentMethods
+                        }))
+                        .value()
+                        
+
+        return {
+            authors: authors,
+            totalLinesOfCode: stats.reduce((totalLines, stat) => totalLines.plus(stat.totalLinesOfCode), new BigNumber(0)).toString(),
+            totalFileCount: stats.reduce((totalFile, stat) => totalFile.plus(stat.totalFileCount), new BigNumber(0)).toString()
+        }
     }
 
 
