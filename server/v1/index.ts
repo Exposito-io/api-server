9500aac9598bfe8b608e4da761df9b2ac0445ade
Mathew Cormier <mathew.corm@gmail.com>
2017-06-30 23:08:31 -0400

    tokens

--- server/v1/index.ts
+++ server/v1/index.ts
@@ -3,6 +3,7 @@ import wallets from './wallets'
 import periodicPayments from './periodic-payments'
 import instances from './instances'
 import repoStats from './repo-stats'
+import tokens from './tokens'
 //import { BitcoinCoreWallet } from '../../models/core/bitcoin-core-wallet'
 
 
@@ -12,6 +13,7 @@ router.get('/', (req, res) => res.json({}))
 router.use('/wallets', wallets)
 router.use('/periodic-payments', periodicPayments)
 router.use('/repo-stats', repoStats)
+router.use('/tokens', tokens)
 
 
 
--- server/v1/tokens.ts
+++ server/v1/tokens.ts
@@ -1,29 +1,21 @@
 import * as express from 'express'
 import * as config from 'config'
 import { GithubStatsProvider } from '../providers/github-stats-provider'
+import { TokenProvider } from '../providers/token-provider'
 
 const githubStatsProvider = new GithubStatsProvider()
+const tokentProvider = new TokenProvider()
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
-        if (req.query.owner && req.query.repo) {
-            let stats = await githubStatsProvider.getRepoStats({
-                owner: req.query.owner,
-                repo: req.query.repo
-            })
-            return res.json(stats)
-        }
-        else if (req.query.repos) {
-            // TODO: validate repos
-            let repos = JSON.parse(req.query.repos)
-            let stats = await githubStatsProvider.getMultipleRepoStats(repos)
-            return res.json(stats)           
-        }
-        else
-            throw('Invalid params')
+        let token = await tokentProvider.generateToken({ userId: 999 })
+        return res.json({ token: token })
     } catch(e) {
         res.json({ error: e})
     }
 })
+
+
+export default router
