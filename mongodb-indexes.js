63d22eaaf3f7b8e678627369a8b3c647e2182e72
Mathew Cormier <mathew.corm@gmail.com>
2017-11-18 21:46:05 -0500

    searchExpositoProjects

--- mongodb-indexes.js
+++ mongodb-indexes.js
@@ -1,1 +1,2 @@
 db.users.createIndex({ name: "text", email: "text"}, { weights: { name: 15, email: 5}})
+db.projects.createIndex({ name: "text", description: "text"}, { weights: { name: 15, description: 5}})
