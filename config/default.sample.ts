a2b95adc70e7d2c2c4103043b7caf0adf52a5520
Mathew Cormier <mathew.corm@gmail.com>
2018-01-21 19:59:28 -0500

    Add config sample

---
+++ config/default.sample.ts
@@ -0,0 +1,17 @@
+import { HostingType } from 'models'
+
+export class Config {
+    serverPort = 3004
+    database = 'mongodb://localhost:27017/exposito'
+    cacheHost = 'localhost'
+    cachePort = 6379
+    cacheDb = 0
+    googleClientWebId = ''
+    googleClientAppId = ''
+    bitcoinNetwork = 'testnet'
+    coreWalletServiceHost = 'http://localhost:3232/bws/api'
+    hosting = HostingType.GoogleCloud
+    corsOrigin = 'http://localhost:3001'
+    secretsPath = 'E:\\tmp\\test2\\Exposito\\secrets'
+    queueServer = 'redis://127.0.0.1:6379'
+}
