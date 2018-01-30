8abb3b48e78af96edcdbc65113a146630783faa5
Mathew Cormier <mathew.corm@gmail.com>
2018-01-29 21:43:12 -0500

    Add EIP20Interface and ExpositoProjectFactory files

--- contracts/DeveloperToken.sol
+++ contracts/DeveloperToken.sol
@@ -1,241 +1,49 @@
 pragma solidity ^0.4.18;
 
-import "./Controlled.sol";
-import "./TokenController.sol";
 
 
 /** 
  *  @title DeveloperToken Contract
- *  @dev      
+ *  @dev    
  */
-contract DeveloperToken is Controlled {
-        
+contract DeveloperToken {        
 
     /** Version of the contract */
     string public version = "0.0.1"; 
 
+    /** Timestamp of the contract creation */
+    uint public creationTimestamp;
 
-    /** 
-     *  @dev `Checkpoint` is the structure that attaches a block number to a
-     *  given value, the block number attached is the one that last changed the value
-     */    
-    struct Checkpoint {
+    /** Total number of tokens */
+    uint256 public totalSupply;
 
-        /** `fromBlock` is the block number that the value was generated from */
-        uint128 fromBlock;
+    /**  */
+    mapping (address => uint256) public balances;
 
-        /** `value` is the amount of tokens at a specific block number */
-        uint128 value;
-    }
+    /** How many decimals to show */
+    uint8 public decimals;                
 
-    /** 
-     * `parentToken` is the Token address that was cloned to produce this token it will be 0x0 for 
-     * a token that was not cloned
-     */
-    DeveloperToken public parentToken;
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
 
-    /** 
-     *  `parentSnapShotBlock` is the block number from the Parent Token that was used to determine 
-     *  the initial distribution of the Clone Token
-     */
-    uint public parentSnapShotBlock;
-
-    /** `creationBlock` is the block number that the Clone Token was created */
-    uint public creationBlock;
-
-    /**
-     * `balances` is the map that tracks the balance of each address, in this contract when the 
-     * balance changes the block number that the change occurred is also included in the map 
-     */
-    mapping (address => Checkpoint[]) balances;
-
-    /** Tracks the history of the `totalSupply` of the token */
-    Checkpoint[] totalSupplyHistory;
-
-
-    /** 
-     * @param _parentToken Address of the parent token, set to 0x0 if it is a
-     *  new token
-     * @param _parentSnapShotBlock Block of the parent token that will
-     *  determine the initial distribution of the clone token, set to 0 if it
-     *  is a new token
-     */
     function DeveloperToken(
-        address _parentToken,
-        uint _parentSnapShotBlock,
-        uint _supply
+        uint256 _supplyAmount,
+        address[] _balanceAddresses,
+        uint256[] _balanceAmounts,
+        uint8 _decimalUnits
     ) public {
-        parentToken = DeveloperToken(_parentToken);
-        parentSnapShotBlock = _parentSnapShotBlock;
-        creationBlock = block.number;
+        require(_balanceAddresses.length == _balanceAmounts.length);
 
-        if (_supply > 0)
-            generateTokens(msg.sender, _supply);
-    }
+        creationTimestamp = now;
+        totalSupply = _supplyAmount;     
+        decimals = _decimalUnits;         
 
-
- 
-
-    /// @param _owner The address that's balance is being requested
-    /// @return The balance of `_owner` at the current block
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
-        return balanceOfAt(_owner, block.number);
-    }
-
-
-    /** Returns the total number of tokens */
-    function totalSupply() public constant returns (uint) {
-        return totalSupplyAt(block.number);
-    }
-
-
-
-    /// @dev Queries the balance of `_owner` at a specific `_blockNumber`
-    /// @param _owner The address from which the balance will be retrieved
-    /// @param _blockNumber The block number when the balance is queried
-    /// @return The balance at `_blockNumber`
-    function balanceOfAt(address _owner, uint _blockNumber) public constant
-        returns (uint) {
-
-        // These next few lines are used when the balance of the token is
-        //  requested before a check point was ever created for this token, it
-        //  requires that the `parentToken.balanceOfAt` be queried at the
-        //  genesis block for that token as this contains initial balance of
-        //  this token
-        if ((balances[_owner].length == 0)
-            || (balances[_owner][0].fromBlock > _blockNumber)) {
-            if (address(parentToken) != 0) {
-                return parentToken.balanceOfAt(_owner, min(_blockNumber, parentSnapShotBlock));
-            } else {
-                // Has no parent
-                return 0;
-            }
-
-        // This will return the expected balance during normal situations
-        } else {
-            return getValueAt(balances[_owner], _blockNumber);
+        for (uint i = 0; i < _balanceAddresses.length; i++) {
+            balances[_balanceAddresses[i]] = _balanceAmounts[i];
         }
     }
 
-    /// @notice Total amount of tokens at a specific `_blockNumber`.
-    /// @param _blockNumber The block number when the totalSupply is queried
-    /// @return The total amount of tokens at `_blockNumber`
-    function totalSupplyAt(uint _blockNumber) public constant returns(uint) {
-
-        // These next few lines are used when the totalSupply of the token is
-        //  requested before a check point was ever created for this token, it
-        //  requires that the `parentToken.totalSupplyAt` be queried at the
-        //  genesis block for this token as that contains totalSupply of this
-        //  token at this block number.
-        if ((totalSupplyHistory.length == 0)
-            || (totalSupplyHistory[0].fromBlock > _blockNumber)) {
-            if (address(parentToken) != 0) {
-                return parentToken.totalSupplyAt(min(_blockNumber, parentSnapShotBlock));
-            } else {
-                return 0;
-            }
-
-        // This will return the expected totalSupply during normal situations
-        } else {
-            return getValueAt(totalSupplyHistory, _blockNumber);
-        }
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
     }
-
-
-
-
-
-
-    /// @notice Generates `_amount` tokens that are assigned to `_owner`
-    /// @param _owner The address that will be assigned the new tokens
-    /// @param _amount The quantity of tokens generated
-    /// @return True if the tokens are generated correctly
-    function generateTokens(address _owner, uint _amount
-    ) public onlyController returns (bool) {
-        uint curTotalSupply = totalSupply();
-        require(curTotalSupply + _amount >= curTotalSupply); // Check for overflow
-        uint previousBalanceTo = balanceOf(_owner);
-        require(previousBalanceTo + _amount >= previousBalanceTo); // Check for overflow
-        updateValueAtNow(totalSupplyHistory, curTotalSupply + _amount);
-        updateValueAtNow(balances[_owner], previousBalanceTo + _amount);
-        return true;
-    }
-
-
-
-
-    /// @dev `getValueAt` retrieves the number of tokens at a given block number
-    /// @param checkpoints The history of values being queried
-    /// @param _block The block number to retrieve the value at
-    /// @return The number of tokens being queried
-    function getValueAt(Checkpoint[] storage checkpoints, uint _block
-    ) constant internal returns (uint) {
-        if (checkpoints.length == 0) 
-            return 0;
-
-        // Shortcut for the actual value
-        if (_block >= checkpoints[checkpoints.length-1].fromBlock)
-            return checkpoints[checkpoints.length-1].value;
-        if (_block < checkpoints[0].fromBlock) return 0;
-
-        // Binary search of the value in the array
-        uint min = 0;
-        uint max = checkpoints.length-1;
-        while (max > min) {
-            uint mid = (max + min + 1) / 2;
-            if (checkpoints[mid].fromBlock<=_block) {
-                min = mid;
-            } else {
-                max = mid-1;
-            }
-        }
-        return checkpoints[min].value;
-    }
-
-    /// @dev `updateValueAtNow` used to update the `balances` map and the
-    ///  `totalSupplyHistory`
-    /// @param checkpoints The history of data being updated
-    /// @param _value The new number of tokens
-    function updateValueAtNow(Checkpoint[] storage checkpoints, uint _value
-    ) internal  {
-        if ((checkpoints.length == 0)
-        || (checkpoints[checkpoints.length - 1].fromBlock < block.number)) {
-               Checkpoint storage newCheckPoint = checkpoints[ checkpoints.length++ ];
-               newCheckPoint.fromBlock =  uint128(block.number);
-               newCheckPoint.value = uint128(_value);
-           } else {
-               Checkpoint storage oldCheckPoint = checkpoints[checkpoints.length-1];
-               oldCheckPoint.value = uint128(_value);
-           }
-    }
-
-    /// @dev Internal function to determine if an address is a contract
-    /// @param _addr The address being queried
-    /// @return True if `_addr` is a contract
-    function isContract(address _addr) constant internal returns(bool) {
-        uint size;
-        if (_addr == 0) 
-            return false;
-        assembly {
-            size := extcodesize(_addr)
-        }
-        return size>0;
-    }
-
-    /// @dev Helper function to return a min betwen the two uints
-    function min(uint a, uint b) pure internal returns (uint) {
-        return a < b ? a : b;
-    }
-
-    /// @notice The fallback function: If the contract's controller has not been
-    ///  set to 0, then the `proxyPayment` method is called which relays the
-    ///  ether and creates tokens as described in the token controller contract
-    function () public payable {
-        require(isContract(controller));
-        require(TokenController(controller).proxyPayment.value(msg.value)(msg.sender));
-    }
-
 
 }
-
-
---
+++ contracts/ExpositoProjectFactory.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.18;
+
+import "./ExpositoProject.sol";
+
+/**
+ *  @dev This contract is used to generate clone contracts from a contract.
+ *  In solidity this is the way to create a contract from a contract of the
+ *  same class
+ */
+contract ExpositoProjectFactory {
+
+    /**
+     *  @notice Update the DApp by creating a new token with new functionalities
+     *  the msg.sender becomes the controller of this clone token
+     * @param _parentToken Address of the token being cloned
+     * @param _snapshotBlock Block of the parent token that will
+     *  determine the initial distribution of the clone token
+     * @return The address of the new token contract
+     */ 
+    function createCloneToken(
+        address _parentToken,
+        uint _snapshotBlock,
+        bool _transfersEnabled,
+        string _projectId
+    ) public returns (ExpositoProject) 
+    {
+        ExpositoProject newToken = new ExpositoProject(
+            this,
+            _parentToken,
+            _snapshotBlock,
+            _projectId,
+            0,
+            0,
+            _transfersEnabled
+            );
+
+        newToken.changeController(msg.sender);
+        return newToken;
+    }
+}
